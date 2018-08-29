b8ee1786885c49ee5bfcb8edf72532c897c251a4
Samith Sandanayake <samiths@zone24x7.com>
2018-08-29 19:20:46 +0530

    Fixed need to ask for notification permissions every time.

--- main.c
+++ main.c
@@ -26,6 +26,7 @@
 
 
 #define COOKIE_FILE ".gchat_ckg"
+#define URL_FOR_GC "https://chat.google.com"
 
 static void quit_cb(GSimpleAction *action, GVariant *parameter, gpointer user_data) {
     g_application_quit(G_APPLICATION(user_data));
@@ -33,6 +34,13 @@ static void quit_cb(GSimpleAction *action, GVariant *parameter, gpointer user_da
 
 static void focus_window_db(GSimpleAction *action, GVariant *parameter, gpointer user_data) {
     gtk_window_deiconify(gtk_application_get_active_window(GTK_APPLICATION(user_data)));
+}
+
+gboolean load_failed_cb(WebKitWebView  *web_view, WebKitLoadEvent load_event, gchar *failing_uri, GError *error, gpointer user_data) {
+    puts("ERROR: cannot load the url, retrying");
+    puts(failing_uri);
+    webkit_web_view_load_uri(web_view, failing_uri);
+    return TRUE;
 }
 
 
@@ -54,6 +62,14 @@ void create_menu(GApplication* application) {
     gtk_application_set_app_menu(GTK_APPLICATION(application),
                                  G_MENU_MODEL(menu));
     g_object_unref(menu);
+}
+
+void initialize_notification_permissions_cb(WebKitWebContext *context, gpointer user_data) {
+    WebKitSecurityOrigin* gc_origin = webkit_security_origin_new_for_uri(URL_FOR_GC);
+    GList* lst = g_list_append(NULL, gc_origin);
+    webkit_web_context_initialize_notification_permissions(context, lst, NULL);
+    g_list_free(lst);
+    webkit_security_origin_unref(gc_origin);
 }
 
 
@@ -139,13 +155,30 @@ static void activate(GApplication *application, gpointer userdata) {
     g_signal_connect(web_view, "show-notification", G_CALLBACK(show_notification), application);
     g_signal_connect(web_view, "permission-request", G_CALLBACK(permission_request), main_window);
     g_signal_connect(web_view, "decide-policy", G_CALLBACK(decide_policy), main_window);
+    g_signal_connect(web_view, "load-failed", G_CALLBACK(load_failed_cb), main_window);
+
+
+    WebKitWebContext* ctx = webkit_web_view_get_context(WEBKIT_WEB_VIEW(web_view));
+    g_signal_connect(ctx, "initialize-notification-permissions", G_CALLBACK(initialize_notification_permissions_cb), main_window);
 
     /* Place the WebKitWebView in the GtkScrolledWindow */
     gtk_container_add(GTK_CONTAINER(scrolled_window), web_view);
     gtk_container_add(GTK_CONTAINER(main_window), scrolled_window);
 
     /* Open a webpage */
-    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), "https://chat.google.com");
+    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), URL_FOR_GC);
+
+    /* inject notification request */
+    WebKitUserContentManager *ucm = webkit_web_view_get_user_content_manager(WEBKIT_WEB_VIEW(web_view));
+    WebKitUserScript* script = webkit_user_script_new(
+            "Notification.requestPermission().then(function(permission) {});",
+            WEBKIT_USER_CONTENT_INJECT_TOP_FRAME,
+            WEBKIT_USER_SCRIPT_INJECT_AT_DOCUMENT_END,
+            NULL,
+            NULL
+    );
+    webkit_user_content_manager_add_script(ucm, script);
+
 
     /* Show */
     GdkGeometry windowProperties;
